c0e6db880410fac77628288a2a6d692aba576267
rakesh-programmer <rakesh.programmer@aol.com>
2025-11-10 20:06:54 +0530

    limit change

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const symbolSchema = new mongoose.Schema({
 });
 const Symbol = mongoose.model('symbol', symbolSchema);
 const calldata = async () => {
-  const result = await Symbol.find().skip(30).limit(15)
+  const result = await Symbol.find().skip(20).limit(10)
   await result.forEach(async (item, index) => {
     const symbol = item['Symbol']
     await scrapeWithCheerio(symbol)
